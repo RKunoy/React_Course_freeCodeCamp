78ffb073464a93d0fe41a3d2c7bb3fe7d5b8b407
Regin Kunoy <s183462@student.dtu.dk>
2021-07-31 20:30:42 +0100

    introduced props and objects

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -5,22 +5,49 @@ import ReactDom from 'react-dom'
 // one dot '.' for each parent folder
 import './index.css'
 
-// function Greeting() {
-//     return <h4>Hello world, welcome to Regin's first component</h4>;
-// }
+// setup variables
+const firstBook = {
+    img:'https://images-na.ssl-images-amazon.com/images/I/51Hj-Lfw+0S._SX322_BO1,204,203,200_.jpg',
+    title: 'Guide to a Better Life',
+    author: 'Gabe Newel'
+};
+
+const secondBook = {
+    img:'https://images-na.ssl-images-amazon.com/images/I/41ItBEAc3kS._SX327_BO1,204,203,200_.jpg',
+    title: 'Nr1 President evur',
+    author: 'Some Redneck'
+};
 
 function BookList() {
-    return <section className='booklist'>
-        <Book/>
-    </section>
+    return ( 
+        <section className='booklist'>
+            <Book
+                img={firstBook.img} 
+                title={firstBook.title} 
+                author={firstBook.author} 
+            >
+                <p>Veniam duis officia do sit. Ipsum deserunt ea anim qui id. Non sint laboris fugiat mollit officia amet enim elit labore aliquip nostrud sint. Tempor elit ea voluptate velit aute aute Lorem.</p>
+            </Book>
+            
+            <Book 
+                img={secondBook.img} 
+                title={secondBook.title} 
+                author={secondBook.author} 
+            ></Book>
+        </section>
+    );
 }
 
-const Book = () => <article className='book'>
-    <img src='https://images-na.ssl-images-amazon.com
-    /images/I/51Hj-Lfw+0S._SX322_BO1,204,203,200_.jpg' alt='' />
-    <h1>Guide to a Better Life</h1>
-    <h4 >Gabe Newel</h4>
-</article>
 
-
+const Book = (props) => {
+    console.log(props);
+    return (
+    <article className='book'>
+        <img src={props.img} alt='' />
+        <h1>{props.title}</h1>
+        <h4>{props.author}</h4>
+        {props.children}
+    </article>
+    );
+};
 ReactDom.render(<BookList/>,document.getElementById('root'));
